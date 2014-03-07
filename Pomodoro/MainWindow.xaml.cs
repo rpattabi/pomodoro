093dab1155469280a900aa7c685198f059276cd7
rpattabi <rpattabi@users.noreply.github.com>
2014-03-07 20:41:27 +0530

    Initial project generated by MVVM light

---
+++ Pomodoro/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Pomodoro.ViewModel;
+
+namespace Pomodoro
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		/// <summary>
+		/// Initializes a new instance of the MainWindow class.
+		/// </summary>
+		public MainWindow()
+		{
+			InitializeComponent();
+			Closing += (s, e) => ViewModelLocator.Cleanup();
+		}
+	}
+}
---
+++ Pomodoro/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pomodoro.Model
+{
+	public interface IDataService
+	{
+		void GetData(Action<DataItem, Exception> callback);
+	}
+}
