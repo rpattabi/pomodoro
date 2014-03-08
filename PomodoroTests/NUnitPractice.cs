09c38ba3b78a3b47c7f4ef327186e18ffcab9e32
rpattabi <rpattabi@users.noreply.github.com>
2014-03-08 05:46:01 +0530

    Added NUnit test project

---
+++ PomodoroTests/NUnitPractice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PomodoroTests
+{
+    [TestFixture]
+    public class NUnitPractice
+    {
+        [Test]
+        public void SumOfTwoNumbers()
+        {
+            Assert.AreEqual(10, 5 + 5);
+        }
+
+        [Test]
+        public void SumOfTwoNumbers2()
+        {
+            Assert.AreEqual(10, 5 + 4);
+        }
+    }
+}
